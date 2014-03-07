8bb99da0c6c4b6104cccff00620256364d7f7cf3
Hamed Qaderi <hamedghaderii@gmail.com>
2014-03-07 14:16:18 +0330

    'change value to text'

--- 1. Accounting/js/custom.js
+++ 1. Accounting/js/custom.js
@@ -1,7 +1,7 @@
 ﻿
 function FireEvent() {
     var select = document.getElementById('sign');
-    var selectedOption = select.options[select.selectedIndex].value;
+    var selectedOption = select.options[select.selectedIndex].text;
     var number = document.getElementById('number').value;
     document.getElementById('result').innerText = number + " " + selectedOption;
 }
