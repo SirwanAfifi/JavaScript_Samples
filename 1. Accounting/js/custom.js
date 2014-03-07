c6518fa01e931b74ca761acd2aa64c6206463937
sir1afifi <sir1afifi@gmail.com>
2014-03-07 01:46:24 -0800

    I've done it

---
+++ 1. Accounting/js/custom.js
@@ -0,0 +1,7 @@
+﻿
+function FireEvent() {
+    var select = document.getElementById('sign');
+    var selectedOption = select.options[select.selectedIndex].value;
+    var number = document.getElementById('number').value;
+    document.getElementById('result').innerText = number + " " + selectedOption;
+}
